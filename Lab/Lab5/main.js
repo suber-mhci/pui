373d45215e6b291e380e32d1db40db99d8175d67
Joshua Suber <joshuasuber@Joshuas-MacBook-Pro.local>
2021-10-04 11:42:13 -0400

    added last javascript functionality

--- Lab/Lab5/main.js
+++ Lab/Lab5/main.js
@@ -17,5 +17,16 @@ function deleteListItem(item) {
      item.parentNode.removeChild(item);
 }
 
+function completeListItem(item) {
+    if (item.checked) { // true if the input checkbox is checked
+        item.parentNode.style.textDecoration = "line-through";
+        // in css, this would be: "text-decoration: line-through"
+    } else {
+        item.parentNode.style.textDecoration = "none";
+        // in css, this would be: "text-decoration: none"
+    }
+}
+
+
 
 
